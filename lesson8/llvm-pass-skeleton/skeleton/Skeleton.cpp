35e8314e2ddbd126fbb39fedf668531593e1430f
Anshuman Mohan <anshumanmohan@live.com>
2022-03-22 15:55:01 -0400

    Making each instruction print to screen

--- lesson8/llvm-pass-skeleton/skeleton/Skeleton.cpp
+++ lesson8/llvm-pass-skeleton/skeleton/Skeleton.cpp
@@ -27,17 +27,27 @@ namespace
       outs() << "I saw a function called " << F.getName() << "\n";
       LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
       int loopCounter = 0;
-      for (LoopInfo::iterator i = LI.begin(), li_end = LI.end(); i != li_end; i++)
+      for (LoopInfo::iterator l = LI.begin(), li_end = LI.end();
+           l != li_end;
+           l++)
       {
-        Loop *L = *i;
+        Loop *L = *l;
         loopCounter++;
-        for (Loop::block_iterator b = L->block_begin(), b_end = L->block_end(); b != b_end; b++)
+        for (Loop::block_iterator b = L->block_begin(), l_end = L->block_end();
+             b != l_end;
+             b++)
         {
-          outs() << "\treporting from loop #" << loopCounter << "\n";
+          outs() << "\tI'm a block inside loop #" << loopCounter << "\n";
+          BasicBlock *B = *b;
+          for (BasicBlock::iterator i = B->begin(), b_end = B->end();
+               i != b_end;
+               i++)
+          {
+            outs() << "\t\tI'm an instruction inside that block\n";
+          }
         }
       }
       outs() << "\tthe function has " << loopCounter << " loop(s) total\n";
-
       return false;
     }
   };
